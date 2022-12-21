269564909aab71536a8708f1fe73c1cbdbc3f88d
suyashgrewal24 <suyashgrewal32@gmail.com>
2022-12-21 16:23:54 +0530

    file and folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { Actors, MovieInformation, Movies, Navbar, Profile } from './components/keep';
+ 
+
+// imported this from style.js 
+import useStyles from "./style"
 const App = () => {
+
+  // using useStyles as hook 
+
+  const classes = useStyles()
   return (
-    <div>
-    App
+    <div className={classes.root}>
+      <CssBaseline />
+      <Router>
+        <Navbar />
+        <main>
+        <div/>
+
+        
+
+        <Routes>
+        
+        <Route path='/' element={<Movies/>}/>
+        <Route path='/movieInformation/:id' element={<MovieInformation/>}/>
+        <Route path='/actors/:id' element={<Actors/>}/>
+        <Route path='/profile/:id' element={<Profile/>}/>
+    
+        </Routes>
+        </main>
+
+      </Router>
+
     </div>
   )
 }
-
 export default App
